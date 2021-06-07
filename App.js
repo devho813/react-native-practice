87e040804ad64db9aa2f26cb85de3bc17c049196
devho813 <devho813@gmail.com>
2021-06-08 01:01:36 +0900

    View, Text 스타일 적용하기

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import {View, Text, StyleSheet} from 'react-native';
 function App() {
   return (
     <View style={styles.mainView}>
-      <View>
+      <View style={styles.subView}>
+        <Text style={styles.mainText}>hello world</Text>
+      </View>
+      <View style={styles.subView}>
         <Text>hello world</Text>
       </View>
-      <View>
-        <Text>hello world</Text>
-      </View>
-      <View>
-        <Text>hello world</Text>
+      <View style={styles.anotherSubView}>
+        <Text style={styles.mainText}>hello world</Text>
       </View>
     </View>
   );
@@ -26,5 +26,25 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  subView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '50%',
+  },
+  anotherSubView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'red',
+    padding: 20,
   },
 });
